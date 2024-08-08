c89e0f10485f6fd5aad7deec583f4836501f8658
graylydia <103657822+graylydia@users.noreply.github.com>
2024-08-07 20:00:34 -0500

    Update app4v2.py

--- app4v2.py
+++ app4v2.py
@@ -8,7 +8,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 
 # Set Hugging Face token
-os.environ["HUGGINGFACE_TOKEN"] = "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"
+os.environ["HUGGINGFACE_TOKEN"] = "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"
 
 # Load the LLM model and tokenizer
 @st.cache_resource
